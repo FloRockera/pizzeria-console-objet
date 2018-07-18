4323256994e574cda661618341d40ff361e88520
Flora Calbris <flora.calbris@gmail.com>
2018-07-18 17:44:19 +0200

    TP5

--- src/testPizza/Pizz.java
+++ src/testPizza/Pizz.java
@@ -1,19 +1,18 @@
-package fr.pizzeria.console;
+package testPizza;
 
 import java.util.Scanner;
 
 import fr.pizzeria.model.Pizza;
 
-public class PizzeriaAdminConsoleApp {
+public class Pizz{
 
 	public static void main(String[] args) {
-
-
+		
 		Scanner scan = new Scanner(System.in);
 
 		int choix = 0;
 
-
+		
 		// Création du tableau avec les objets pizza
 		Pizza[] pizzas = new Pizza[100];
 		pizzas[0] = new Pizza(0,"PEP","Pépéroni",12.50);
@@ -24,67 +23,36 @@ public class PizzeriaAdminConsoleApp {
 		pizzas[5] = new Pizza(5,"SAV","La savoyarde",13.00);
 		pizzas[6] = new Pizza(6,"ORI","L'orientale",13.50);
 		pizzas[7] = new Pizza(7,"IND","L'indienne",14.00);
-
-
+		
+		
 		// Saisir un choix sur le menu principal
 		do {
 			System.out.println("***** Pizzeria Administration ***** \n 1. Lister les pizzas \n 2. Ajouter une nouvelle pizza \n 3. Mettre à jour une pizza \n 4. Supprimer une pizza \n 99. Sortir");
 			choix = scan.nextInt();
 
 			switch (choix) {
-
-			//Affiche la liste sans les null
 			case 1:
 				System.out.println("Liste des pizzas");
-				for (int i=0; i<pizzas.length;i++){
-					if(pizzas[i]!=null){
-						System.out.println(pizzas[i].getCode()+" -> "+ pizzas[i].getLibelle()+" ( "+pizzas[i].getPrix()+" ) ");
-					}
-				}
 				break;
-
-				//Ajout d'une nouvelle pizza dans le tableau existant	
 			case 2:
 				System.out.println("Ajout d’une nouvelle pizza");
-				System.out.println("Veuillez saisir le code : ");
-				String code = scan.next();
-				System.out.println("Veuillez saisir le nom (sans espace) : ");
-				String libelle = scan.next();
-				System.out.println("Veuillez saisir le prix : ");
-				double prix = scan.nextInt();
-				for (int i=0; i<pizzas.length;i++){
-					if(pizzas[i]==null){
-						pizzas[i] = new Pizza(0, code, libelle, prix);
-						break;
-					}
-				}
 				break;
-
 			case 3:
 				System.out.println("Mise à jour d’une pizza");
 				break;
-
 			case 4:
 				System.out.println("Suppression d’une pizza");
 				break;
-
-			case 99:
-				System.out.println("Aurevoir");
-				break;
-
 			default:
 				System.out.println("Cette option de menu n'existe pas");
 				break;
-
 			}
 		} while (choix != 99);
-
+		System.out.println("Aurevoir");
 
 		scan.close();
-
-
-
-
+		
+		
 	}
 
 }
